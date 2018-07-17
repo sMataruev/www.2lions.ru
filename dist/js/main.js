4efa2ad936ce53d204a2aad9b1d9663a8120628d
Mataruev Sergey <smataruev@gmail.com>
2018-07-17 22:30:28 +0300

    Переделали wrapper

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,46 @@
-$().ready(() => {
+ymaps.ready(init);
 
-    let blockItem = $('.block-item');
-    let blockImag = $('.block-item__image');
-    let itemOverlay = $('.block-item__overlay');
-    let headerText = $('.block-item__header-text ');
-    let blockItemText = $('.block-item__body-text');
+function init() {
+    let myMap;
+    let TwoLions;
+    let myPin;
+    let myStyleMap;
+    myMap = new ymaps.Map("myMap", {
+        center: [48.0170, 37.8113],
+        zoom: 17,
+        controls: []
+    });
+    myMap.behaviors.disable([
+        // 'drag',
+        // 'scrollZoom',
+        'dblClickZoom',
+    ]);
 
-    blockItem.mouseover(() => {
-       itemOverlay.addClass('block-item__overlay-hide');
-        blockItemText.addClass('headerBlur');
-        headerText.addClass('headerBlur');
-        blockImag.addClass('block-item__image_blur');
-    });
-    blockItem.mouseout(() => {
-        blockImag.removeClass('block-item__image_blur');
-        itemOverlay.removeClass('block-item__overlay-hide');
-        blockItemText.removeClass('headerBlur');
-        headerText.removeClass('headerBlur');
-    });
-});
+    myStyleMap = `<div class="myMapStyle">
+                <h3 class="mapHeaderText">
+                ПОЛИГРАФИЯ <br> РЕКЛАМА <br>БРЕНДИНГ<br></h3>
+                <img  class="mapLogo" src="../img/Icons/lion_log1.svg">
+                <div class="mapLine"></div>
+                <p class="mapDesc">ЛЮБОЙ СЛОЖНОСТИ</p>
+        </div>`;
+
+
+    TwoLions = new ymaps.Placemark([48.0170, 37.8113], {
+            balloonContentHeader: myStyleMap,
+            balloonContentBody: '<span class="mapBody">Мы делаем качественную рекламу для Вас</span>',
+            balloonContentFooter: '<span class="mapBodyTel">+38 (071) 111 11 11 <br> +38 (071) 111 11 11 </span>',
+            hintContent: ''
+        },
+        {
+            iconLayout: 'default#image',
+            iconImageHref: '../img/Icons/maps-and-flags.svg',
+            iconImageSize: [40, 80],
+            iconImageOffset: [-20, -65]
+        });
+
+    myMap.geoObjects.add(TwoLions);
+}
+
+
+
+
